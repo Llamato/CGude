b4a488a11300481a942236569a1beea136030063
llamato <tguessbacher@gmx.de>
2026-09-08 23:24:20 +0200

    Added way to much sauce

--- 06_Phong/main.cpp
+++ 06_Phong/main.cpp
@@ -18,19 +18,68 @@ public:
 
   MyGLApp() : GLApp{ 600, 600, 1, "Phong Lighting" } {}
 
+  Vec3 htmlColorToOpenGlColor(const std::string html) {
+    std::string htmlR = html.substr(1, 2);
+    std::string htmlG = html.substr(3, 2);
+    std::string htmlB = html.substr(5, 2);
+    unsigned int absoluteR = std::stoul(htmlR, nullptr, 16);
+    unsigned int absoluteG = std::stoul(htmlG, nullptr, 16);
+    unsigned int absoluteB = std::stoul(htmlB, nullptr, 16);
+    float relativeR = static_cast<float>(absoluteR) / UINT8_MAX;
+    float relativeG = static_cast<float>(absoluteG) / UINT8_MAX;
+    float relativeB = static_cast<float>(absoluteB) / UINT8_MAX; 
+    return Vec3{relativeR, relativeG, relativeB};
+  }
+
+  std::vector<Vec3> getDegenerateColors(void) {
+    Vec3 blue = htmlColorToOpenGlColor("#5BCFFA");
+    Vec3 pink = htmlColorToOpenGlColor("#F5A9B8");
+    Vec3 white = Vec3{1.0f, 1.0f, 1.0f};
+    std::vector<Vec3> colors;
+    colors.push_back(blue);
+    colors.push_back(pink);
+    colors.push_back(white);
+    colors.push_back(pink);
+    colors.push_back(blue);
+    return colors;
+  }
+
+    
+  Vec3 rotateColorWheel(std::vector<Vec3> colors) {
+    static size_t currentWheelColor = 0;
+    if(currentWheelColor == colors.size()) {
+      currentWheelColor = 0;
+    }
+    currentWheelColor++;
+    return colors[currentWheelColor];
+  }
+
+  Material degenMaterial() {
+    const static std::vector<Vec3> degenColors = getDegenerateColors();
+    const Vec3 color = rotateColorWheel(degenColors);
+    Material material{color, color, color};
+    return material;
+  }
+
   virtual void init() override {
     GL(glDisable(GL_CULL_FACE));
 
     // define some materials for our triangles
-    const Material red{ Vec3{1, 0, 0} };
-    const Material green{ Vec3{0, 1, 0} };
-    const Material blue{ Vec3{0, 0, 1} };
-    const Material lightOrange{ Vec3{1, 0.765f, 0.482f } };
+    Material red{ Vec3{1, 0, 0} };
+    Material green{ Vec3{0, 1, 0} };
+    Material blue{ Vec3{0, 0, 1} };
+    Material lightOrange{ Vec3{1, 0.765f, 0.482f } };
 
     // define the vertices for the first triangle
-    const Vertex v0{ Vec3{12, 360, 0}, red };
-    const Vertex v1{ Vec3{240, 588, 0}, green };
-    const Vertex v2{ Vec3{444, 48, 0}, blue };
+    Vertex v0{ Vec3{12, 360, 0}, red };
+    Vertex v1{ Vec3{240, 588, 0}, green };
+    Vertex v2{ Vec3{444, 48, 0}, blue };
+
+    #ifdef SAUCE
+    v0.material = degenMaterial();
+    v1.material = degenMaterial();
+    v2.material = degenMaterial();
+    #endif
 
     // create a simple shader for the first triangle
     const AmbientShader as{};
@@ -40,9 +89,15 @@ public:
     triangle1.draw(image);
 
     // define new vertices for the second triangle
-    const Vertex d0{ Vec3{12, 384, 0}, lightOrange };
-    const Vertex d1{ Vec3{216, 588, 0}, lightOrange };
-    const Vertex d2{ Vec3{12, 588, 0}, lightOrange };
+    Vertex d0{ Vec3{12, 384, 0}, lightOrange };
+    Vertex d1{ Vec3{216, 588, 0}, lightOrange };
+    Vertex d2{ Vec3{12, 588, 0}, lightOrange };
+
+    #if SAUCE
+      d0.material = degenMaterial();
+      d1.material = degenMaterial();
+      d2.material = degenMaterial();
+    #endif
 
     // create a diffuse shader for the second triangle
     Vec3 lightPos{ 12, 486, 50 };
@@ -54,7 +109,11 @@ public:
     triangle2.draw(image);
 
     // create a new vertex for the third triangle
-    const Vertex v3{ Vec3{36, 60, 0}, blue };
+    Vertex v3{ Vec3{36, 60, 0}, blue };
+
+    #ifdef SAUCE
+      v3.material = degenMaterial();
+    #endif
 
     // create a phong shader for the third triangle with new light coefficients
     const Vec3 viewer{ image.width / 2.0f, image.height / 2.0f, 500 };
@@ -69,9 +128,15 @@ public:
     triangle3.draw(image);
 
     // define new vertices with custom normals for the fourth triangle
-    const Vertex a{ Vec3{264, 588, 0}, red, Vec3{-1, -1, 1} };
-    const Vertex b{ Vec3{588, 588, 0}, red, Vec3{0.5f, 0.5f, 1} };
-    const Vertex c{ Vec3{588, 372, 0}, red, Vec3{-1, -1, 1} };
+    Vertex a{ Vec3{264, 588, 0}, red, Vec3{-1, -1, 1} };
+    Vertex b{ Vec3{588, 588, 0}, red, Vec3{0.5f, 0.5f, 1} };
+    Vertex c{ Vec3{588, 372, 0}, red, Vec3{-1, -1, 1} };
+
+    #ifdef SAUCE
+      a.material = degenMaterial();
+      b.material = degenMaterial();
+      c.material = degenMaterial(); 
+    #endif
 
     // create and draw the red phong shaded triangle
     Triangle triangle4(a, b, c, phongShader);
@@ -79,6 +144,10 @@ public:
 
     // define a new vertex for the fifth triangle
     Vertex v4(Vec3(588, 360, 0), green);
+
+    #ifdef SAUCE
+      v4.material = degenMaterial();
+    #endif
 
     // create a bump shader for the fifth triangle
     BumpPhongShader bump(phongShader, 50, 0.3f);
@@ -90,6 +159,7 @@ public:
     baseVertex2 = v4;
     baseVertex3 = v2;
     triangle5.draw(image);
+    
   }
 
   virtual void draw() override {
@@ -100,7 +170,7 @@ public:
     return std::fmaxf(1 - std::fabsf(t), 0.0f);
   }
 
-#ifdef SAUCE
+#ifdef EXTRA
   virtual void animate(double animationTime) override {
     const float maxCellSize = 200.0f;
     const float maxBumpHeight = 1.0f;
