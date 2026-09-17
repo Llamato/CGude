ba5d4d089d401f3fdf9ee596c2652fe73eba4b31
llamato <tguessbacher@gmx.de>
2026-09-17 23:42:34 +0200

    added solutin for 11

--- 11_Triforce/main.cpp
+++ 11_Triforce/main.cpp
@@ -10,6 +10,7 @@
 #define FLOATS_PER_TRIANGLE VERTICIES_PER_TRIANGLE * FLOATS_PER_VERTEX
 
 #define ROTATION_PERIOD 10
+#define TRIANGLE_HEIGHT 0.66f
 
 #define VERTEX_X_OFFSET 0
 #define VERTEX_Y_OFFSET 1
@@ -17,7 +18,6 @@
 #define COLOR_R_OFFSET 3
 #define COLOR_G_OFFSET 4
 #define COLOR_B_OFFSET 5
-
 
 class MyGLApp : public GLApp {
 public:
@@ -29,21 +29,26 @@ public:
   GLuint vbos{0};
   GLuint vaos{0};
   
-  GLfloat vertexData[NUM_TRIANGLES * VERTICIES_PER_TRIANGLE * FLOATS_PER_VERTEX];
-  Vec3 triangleCenterPoints[NUM_TRIANGLES];
+  struct TriangleAttributes {
+    Vec3 centerPoint;
+    Vec3 rotationAxis;
+    Vec3 vertexColors[VERTICIES_PER_TRIANGLE];
+  };
 
-  void debugPrintVertexArray() {
-    std::cout << "X\tY\tZ\tR\tG\tB\n";
-    for(size_t currentTriangle = 0; currentTriangle < NUM_TRIANGLES; currentTriangle++) {
-      for(size_t currentVertex = 0; currentVertex < VERTICIES_PER_TRIANGLE; currentVertex++) {
-        for(size_t currentFloat = 0; currentFloat < FLOATS_PER_VERTEX; currentFloat++) {
-          std::cout << vertexData[currentTriangle * VERTICIES_PER_TRIANGLE + currentVertex * FLOATS_PER_VERTEX + currentFloat] << '\t';
-        }
-        std::cout << '\n';
-      }
-      std::cout << "\n";
-    }
-  }
+  const Vec3 colorRed = htmlColorToOpenGlColor("#FF0000");
+  const Vec3 colorMagenta = htmlColorToOpenGlColor("#FF00FF");
+  const Vec3 colorOrange = htmlColorToOpenGlColor("#FFAA00");
+  const Vec3 colorTurquesa = htmlColorToOpenGlColor("#00FFFF");
+  const Vec3 colorBlue = htmlColorToOpenGlColor("#0000FF");
+  const Vec3 colorYellow = htmlColorToOpenGlColor("#FFFF00");
+  const Vec3 colorGreen = htmlColorToOpenGlColor("#00FF00");
+  
+  GLfloat vertexData[NUM_TRIANGLES * VERTICIES_PER_TRIANGLE * FLOATS_PER_VERTEX];
+  TriangleAttributes triangleData[NUM_TRIANGLES] = {
+    { Vec3{0.0f, 1.0f, 0},    Vec3{1.0f, 0.0f, 0.0f}, {colorRed, colorMagenta, colorOrange} },
+    { Vec3{-0.5f, 0.0f, 0},   Vec3{0.0f, 1.0f, 0.0f}, {colorMagenta, colorBlue, colorTurquesa} },
+    { Vec3{0.5f, 0.0f, 0},    Vec3{0.0f, 0.0f, 1.0f}, {colorYellow, colorGreen, colorTurquesa} },
+  };
 
   Vec3 htmlColorToOpenGlColor(const std::string html) {
     std::string htmlR = html.substr(1, 2);
@@ -70,40 +75,63 @@ public:
     setVertexColor(verticies, triangleStart + 2 * FLOATS_PER_VERTEX, colorC);
   }
 
-  void generateEquilateralTriangle(GLfloat* vertices, size_t start, size_t stride, Vec3 center, float height){
+  void generateEquilateralTriangle(GLfloat* vertices, size_t start, size_t stride, Vec3 center, float height, Vec3 eulerAngles){
+    //Construction
     const float halfBase = height / std::sqrt(3.0f);
-    Vec3 A = center + Vec3{ 0.0f,        height * 0.5f,  0.0f };
-    Vec3 B = center + Vec3{ -halfBase,  -height * 0.5f,  0.0f };
-    Vec3 C = center + Vec3{  halfBase,  -height * 0.5f,  0.0f };
-    vertices[start + VERTEX_X_OFFSET] = A.x;
-    vertices[start + VERTEX_Y_OFFSET] = A.y;
-    vertices[start + VERTEX_Z_OFFSET] = A.z;
-    vertices[start + stride + VERTEX_X_OFFSET] = B.x;
-    vertices[start + stride + VERTEX_Y_OFFSET] = B.y;
-    vertices[start + stride + VERTEX_Z_OFFSET] = B.z;
-    vertices[start + 2 * stride + VERTEX_X_OFFSET] = C.x;
-    vertices[start + 2 * stride + VERTEX_Y_OFFSET] = C.y;
-    vertices[start + 2 * stride + VERTEX_Z_OFFSET] = C.z;
+    Vec3 Alocal = Vec3{ 0.0f,        height * 0.5f,  0.0f };
+    Vec3 Blocal = Vec3{ -halfBase,  -height * 0.5f,  0.0f };
+    Vec3 Clocal = Vec3{  halfBase,  -height * 0.5f,  0.0f };
+    
+    //Rotation
+    const Mat4 Rx = {
+      1, 0, 0, 0,
+      0, std::cosf(eulerAngles.x), -std::sinf(eulerAngles.x), 0,
+      0, std::sinf(eulerAngles.x), std::cosf(eulerAngles.x), 0,
+      0, 0, 0, 1
+    };
+    const Mat4 Ry = {
+      std::cosf(eulerAngles.y), 0, std::sinf(eulerAngles.y), 0,
+      0, 1, 0, 0,
+      -std::sinf(eulerAngles.y), 0, std::cosf(eulerAngles.y), 0,
+      0, 0, 0, 1
+    };
+    const Mat4 Rz = {
+      std::cosf(eulerAngles.z), -std::sinf(eulerAngles.z), 0, 0,
+      std::sinf(eulerAngles.z), std::cosf(eulerAngles.z), 0, 0,
+      0, 0, 1, 0,
+      0, 0, 0, 1
+    };
+    const Mat4 Reuler = Rx * Ry * Rz;
+    Vec4 Arotated = Reuler * Vec4{Alocal, 1.0f};
+    Vec4 Brotated = Reuler * Vec4{Blocal, 1.0f};
+    Vec4 Crotated = Reuler * Vec4{Clocal, 1.0f};
+
+    //Translation
+    Vec3 Aglobal = center + Arotated.xyz;
+    Vec3 Bglobal = center + Brotated.xyz;
+    Vec3 Cglobal = center + Crotated.xyz;
+
+    //Assembly
+    vertices[start + VERTEX_X_OFFSET] = Aglobal.x;
+    vertices[start + VERTEX_Y_OFFSET] = Aglobal.y;
+    vertices[start + VERTEX_Z_OFFSET] = Aglobal.z;
+    vertices[start + stride + VERTEX_X_OFFSET] = Bglobal.x;
+    vertices[start + stride + VERTEX_Y_OFFSET] = Bglobal.y;
+    vertices[start + stride + VERTEX_Z_OFFSET] = Bglobal.z;
+    vertices[start + 2 * stride + VERTEX_X_OFFSET] = Cglobal.x;
+    vertices[start + 2 * stride + VERTEX_Y_OFFSET] = Cglobal.y;
+    vertices[start + 2 * stride + VERTEX_Z_OFFSET] = Cglobal.z;
 }
 
 void setupTriangles() {
-  const Vec3 colorRed = htmlColorToOpenGlColor("#FF0000");
-  const Vec3 colorMagenta = htmlColorToOpenGlColor("#FF00FF");
-  const Vec3 colorOrange = htmlColorToOpenGlColor("#FFAA00");
-  const Vec3 colorTurquesa = htmlColorToOpenGlColor("#00FFFF");
-  const Vec3 colorBlue = htmlColorToOpenGlColor("#0000FF");
-  const Vec3 colorYellow = htmlColorToOpenGlColor("#FFFF00");
-  const Vec3 colorGreen = htmlColorToOpenGlColor("#00FF00");
-  setTriangleColors(vertexData, 0, colorRed, colorMagenta, colorOrange);
-  setTriangleColors(vertexData, FLOATS_PER_TRIANGLE, colorMagenta, colorBlue, colorTurquesa);
-  setTriangleColors(vertexData, 2 * FLOATS_PER_TRIANGLE, colorYellow, colorGreen, colorTurquesa);
+  for(size_t currentTriangle = 0; currentTriangle < NUM_TRIANGLES; currentTriangle++) {
+    generateEquilateralTriangle(vertexData, currentTriangle * FLOATS_PER_TRIANGLE, FLOATS_PER_VERTEX, triangleData[currentTriangle].centerPoint, TRIANGLE_HEIGHT, triangleData[currentTriangle].rotationAxis);
+    setTriangleColors(vertexData, currentTriangle * FLOATS_PER_TRIANGLE, triangleData[currentTriangle].vertexColors[0], triangleData[currentTriangle].vertexColors[1], triangleData[currentTriangle].vertexColors[2]);
+  }
+}
 
-  triangleCenterPoints[0] = Vec3{-0.5f , 0.0f, 0};
-  generateEquilateralTriangle(vertexData, FLOATS_PER_TRIANGLE,  FLOATS_PER_VERTEX, triangleCenterPoints[0], .66f);
-  triangleCenterPoints[1] = Vec3{0.0f , 1.0f, 0};
-  generateEquilateralTriangle(vertexData, 0, FLOATS_PER_VERTEX, triangleCenterPoints[1], .66f);
-  triangleCenterPoints[2] = Vec3{0.5f , 0.0f, 0};
-  generateEquilateralTriangle(vertexData, 2 * FLOATS_PER_TRIANGLE, FLOATS_PER_VERTEX, triangleCenterPoints[2], .66f);
+Vec3 vec3fmodf(Vec3 vec, float f) {
+  return Vec3{std::fmodf(vec.x, f), std::fmodf(vec.y, f), std::fmodf(vec.z, f)};
 }
   
   MyGLApp()
@@ -117,7 +145,16 @@ void setupTriangles() {
   }
 
   virtual void animate(double animationTime) override {
-    
+    //update data in cpu memory
+    const double rotationAngle = std::fmod(animationTime, 2 * M_PI);
+    for(size_t currentTriangle = 0; currentTriangle < NUM_TRIANGLES; currentTriangle++) {
+      generateEquilateralTriangle(vertexData, currentTriangle * FLOATS_PER_TRIANGLE, FLOATS_PER_VERTEX, triangleData[currentTriangle].centerPoint, TRIANGLE_HEIGHT, triangleData[currentTriangle].rotationAxis * rotationAngle);
+    }
+
+    //Send updated data to gpu
+    GL(glBindBuffer(GL_ARRAY_BUFFER, vbos));
+    GL(glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vertexData), vertexData));
+    GL(glBindBuffer(GL_ARRAY_BUFFER, 0));
   }
 
   virtual void draw() override {
